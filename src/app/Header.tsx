40479d1753acc557b79d0c18533f62036360f0b8
Jishnu <jpjishnu21@gmail.com>
2025-06-07 12:43:19 +0530

    logo change

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -79,28 +79,28 @@ const Header = () => {
       <div 
         className={`flex items-center bg-[#e6e6e6] text-[14px] text-[#222] transition-all duration-300 overflow-hidden ${topBarVisible ? 'h-[38px] md:h-[38px] opacity-100' : 'h-0 opacity-0'}`}
       >
-        <div className="container mx-auto flex flex-wrap justify-between">
+        <div className="container px-4 md:px-6 lg:px-8 mx-auto flex flex-wrap justify-between">
           <div className="flex items-center gap-2">
             <span className="flex items-center text-[#222]">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
               </svg>
             </span>
-            <span className="text-xs md:text-sm">info@sspl.net.in</span>
+            <span className="text-xs  md:text-sm">info@sspl.net.in</span>
           </div>
-          <div className="flex items-center gap-2 ml-4 md:ml-8">
+          <div className="flex items-center  gap-2 ml-4 md:ml-8">
             <span className="flex items-center text-[#222]">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
               </svg>
             </span>
-            <span className="text-xs md:text-sm">+91-11-23765368</span>
+            <span className="text-xs md:text-sm ">+91-11-23765368</span>
           </div>
         </div>
       </div>
       {/* Main Navigation */}
       <div className="bg-white h-[65px] md:h-[65px] lg:h-[70px] border-b border-[#eee] shadow-sm">
-        <div className="container mx-auto  h-full flex items-center justify-between">
+        <div className="container mx-auto h-full flex items-center justify-between px-4 md:px-6 lg:px-8">
           <div className="flex items-center overflow-hidden">
             <Link href="/" className="flex items-center">
               <Image src="/images/logos/logo2.jpg" alt="SSPL Logo" layout="intrinsic" width={140} height={40} className="py-1 md:w-auto lg:w-auto max-h-[40px]" />
