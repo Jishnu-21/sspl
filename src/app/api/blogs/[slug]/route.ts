a91326ee07e4716f471b04b42053ff7410e73a9f
Jishnu <jpjishnu21@gmail.com>
2025-07-21 11:05:55 +0530

    error fix

--- src/app/api/blogs/[slug]/route.ts
+++ src/app/api/blogs/[slug]/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { BASE_URL } from '../../../config/endpoint';
 
-export async function DELETE(req: NextRequest, { params }: { params: { slug: string } }) {
+export async function DELETE(req: NextRequest, context: { params: any }) {
   try {
-    const { slug } = params;
+    const { slug } = context.params;
     const backendRes = await fetch(`http://${BASE_URL}/api/blogs/${slug}`, { method: 'DELETE' });
     const data = await backendRes.json();
     return new NextResponse(JSON.stringify(data), {
@@ -18,9 +18,9 @@ export async function DELETE(req: NextRequest, { params }: { params: { slug: str
   }
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { slug: string } }) {
+export async function PUT(req: NextRequest, context: { params: any }) {
   try {
-    const { slug } = params;
+    const { slug } = context.params;
     const formData = await req.formData();
     const backendRes = await fetch(`http://${BASE_URL}/api/blogs/${slug}`, {
       method: 'PUT',
